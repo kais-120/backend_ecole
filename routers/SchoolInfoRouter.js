17f98d350e9f831fb319ca009ab5a268913daaf6
kais-120 <kaisbennour7@gmail.com>
2026-09-02 01:54:56 +0100

    try to finish

--- routers/SchoolInfoRouter.js
+++ routers/SchoolInfoRouter.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const {
   getSchoolInfo,
   updateSchoolInfo,
-} = require("../controllers/schoolInfoController");
+} = require("../controllers/SchoolInfoController");
 
 router.get("/", getSchoolInfo);
 
